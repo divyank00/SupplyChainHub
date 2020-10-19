de8ad2eef1f819e3f63c7d26c620db7e124dce1d
divyank00 <lunkaddivyank@gmail.com>
2020-10-19 22:36:23 +0530

    Reformat Code

--- SmartContract/app/src/main/java/com/example/smartcontract/callFunction.java
+++ SmartContract/app/src/main/java/com/example/smartcontract/callFunction.java
@@ -38,6 +38,7 @@ import org.web3j.abi.datatypes.generated.Int256;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
+import org.web3j.exceptions.MessageDecodingException;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.Transaction;
@@ -157,22 +158,28 @@ public class callFunction extends AppCompatActivity {
                                     });
                                     break;
                                 case "address":
-                                    outputAsync.add(new TypeReference<Address>() {});
+                                    outputAsync.add(new TypeReference<Address>() {
+                                    });
                                     break;
                                 case "uint256":
-                                    outputAsync.add(new TypeReference<Uint256>() {});
+                                    outputAsync.add(new TypeReference<Uint256>() {
+                                    });
                                     break;
                                 case "int256":
-                                    outputAsync.add(new TypeReference<Int256>() {});
+                                    outputAsync.add(new TypeReference<Int256>() {
+                                    });
                                     break;
                                 case "string[]":
-                                    outputAsync.add(new TypeReference<DynamicArray<Utf8String>>() {});
+                                    outputAsync.add(new TypeReference<DynamicArray<Utf8String>>() {
+                                    });
                                     break;
                                 case "address[]":
-                                    outputAsync.add(new TypeReference<DynamicArray<Address>>() {});
+                                    outputAsync.add(new TypeReference<DynamicArray<Address>>() {
+                                    });
                                     break;
                                 case "tuple":
-                                    outputAsync.add(new TypeReference<org.web3j.abi.datatypes.DynamicStruct>() {});
+                                    outputAsync.add(new TypeReference<org.web3j.abi.datatypes.DynamicStruct>() {
+                                    });
                                     break;
                             }
                         }
@@ -186,7 +193,7 @@ public class callFunction extends AppCompatActivity {
                             calculateGas calculateGas = new calculateGas();
                             calculateGas.execute();
                         }
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         Toast.makeText(callFunction.this, e.toString(), Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -228,7 +235,7 @@ public class callFunction extends AppCompatActivity {
                 Function function = new Function(obj.optString("name"), // Function name
                         inputAsync,  // Function input parameters
                         outputAsync); // Function returned parameters
-                Log.d("Address Output: ",outputAsync.size()+"");
+                Log.d("Address Output: ", outputAsync.size() + "");
                 String encodedFunction = FunctionEncoder.encode(function);
                 EthCall ethCall = web3j.ethCall(
                         Transaction.createEthCallTransaction(credentials.getAddress(), contractAddress, encodedFunction),
@@ -289,23 +296,31 @@ public class callFunction extends AppCompatActivity {
                 Function function = new Function(obj.optString("name"), // Function name
                         inputAsync,  // Function input parameters
                         outputAsync); // Function returned parameters
-                Log.d("Address Output: ",outputAsync.size()+"");
+                Log.d("Address Output: ", outputAsync.size() + "");
                 String txData = FunctionEncoder.encode(function);
 
-                Log.d("Address Public: ",credentials.getAddress()+"");
-                Log.d("Address Encode: ",txData+"");
-                Log.d("Address Contract: ",contractAddress+"");
-                Transaction t = Transaction.createEthCallTransaction(credentials.getAddress(),contractAddress,txData);
+                Log.d("Address Public: ", credentials.getAddress() + "");
+                Log.d("Address Encode: ", txData + "");
+                Log.d("Address Contract: ", contractAddress + "");
+                Transaction t = Transaction.createEthCallTransaction(credentials.getAddress(), contractAddress, txData);
                 BigInteger val = web3j.ethEstimateGas(t).send().getAmountUsed();
-                Log.d("Address Gas Used: ",val+"");
-                Log.d("Address Gas Price: ", ""+4.1*1e-9+ " Ether");
-                Double txnFee = val.intValue()*4.1*1e-9;
+                Log.d("Address Gas Used: ", val + "");
+                Log.d("Address Gas Price: ", "" + 4.1 * 1e-9 + " Ether");
+                Double txnFee = val.intValue() * 4.1 * 1e-9;
                 DecimalFormat df = new DecimalFormat("#.##########");
                 df.setRoundingMode(RoundingMode.CEILING);
                 result = df.format(txnFee) + " Ether";
-                Log.d("Address Fee: ",result);
-            } catch (Exception e) {
-                Log.d("Address Error: ", e.toString());
+                Log.d("Address Fee: ", result);
+            } catch (MessageDecodingException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(callFunction.this, "You don't have desired permissions!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d("Address Error: ",e.toString());
                 e.printStackTrace();
             }
             return result;
@@ -315,26 +330,28 @@ public class callFunction extends AppCompatActivity {
         protected void onPostExecute(String txnFee) {
             super.onPostExecute(txnFee);
             progressHUD.dismiss();
-            MaterialDialog mDialog = new MaterialDialog.Builder(callFunction.this)
-                    .setTitle("Confirm Transaction?")
-                    .setMessage(txnFee + " will be deducted!")
-                    .setCancelable(false)
-                    .setPositiveButton("Confirm", new MaterialDialog.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int which) {
-                            dialogInterface.dismiss();
-                            write write = new write();
-                            write.execute();
-                        }
-                    })
-                    .setNegativeButton("Cancel", new MaterialDialog.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int which) {
-                            dialogInterface.dismiss();
-                        }
-                    })
-                    .build();
-            mDialog.show();
+            if (!txnFee.isEmpty()) {
+                MaterialDialog mDialog = new MaterialDialog.Builder(callFunction.this)
+                        .setTitle("Confirm Transaction?")
+                        .setMessage(txnFee + " will be deducted!")
+                        .setCancelable(false)
+                        .setPositiveButton("Confirm", new MaterialDialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                dialogInterface.dismiss();
+                                write write = new write();
+                                write.execute();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new MaterialDialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .build();
+                mDialog.show();
+            }
         }
     }
 
@@ -369,9 +386,9 @@ public class callFunction extends AppCompatActivity {
                 Function function = new Function(obj.optString("name"), // Function name
                         inputAsync,  // Function input parameters
                         outputAsync); // Function returned parameters
-                Log.d("Address Output: ",outputAsync.size()+"");
+                Log.d("Address Output: ", outputAsync.size() + "");
                 String txData = FunctionEncoder.encode(function);
-                Log.d("Address Encode W",txData);
+                Log.d("Address Encode W", txData);
                 TransactionManager txManager = new FastRawTransactionManager(web3j, credentials);
 
                 // Send transaction
@@ -391,9 +408,9 @@ public class callFunction extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(txReceipt.isStatusOK()){
+                        if (txReceipt.isStatusOK()) {
                             animationView.setAnimation(R.raw.success);
-                        }else{
+                        } else {
                             animationView.setAnimation(R.raw.error);
                         }
                         animationView.setVisibility(View.VISIBLE);
@@ -413,9 +430,9 @@ public class callFunction extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             progressHUD.dismiss();
-            if(!result.isEmpty()){
+            if (!result.isEmpty()) {
                 outputTV.setText("Transaction Hash: ");
-                outputResult.setText(result+"\n\nhttps://rinkeby.etherscan.io/tx/"+result);
+                outputResult.setText(result + "\n\nhttps://rinkeby.etherscan.io/tx/" + result);
                 outputTV.setVisibility(View.VISIBLE);
                 outputResult.setVisibility(View.VISIBLE);
             }
@@ -425,7 +442,7 @@ public class callFunction extends AppCompatActivity {
     public void hideKeyboard() {
         View view = getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
--- SmartContract/app/src/main/java/com/example/smartcontract/data.java
+++ SmartContract/app/src/main/java/com/example/smartcontract/data.java
@@ -1,6 +1,18 @@
 package com.example.smartcontract;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class data {
     public static String privateKey;
     public static String publicKey;
+    public static Map<Integer, String> State = new HashMap<Integer, String>() {{
+        put(0, "Assembling");
+        put(1, "Made");
+        put(2, "Packed");
+        put(3, "ForSale");
+        put(4, "Sold");
+        put(5, "Shipped");
+        put(6, "Received");
+    }};
 }
